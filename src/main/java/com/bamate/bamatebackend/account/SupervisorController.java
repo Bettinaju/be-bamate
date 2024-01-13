aeaa4791d7eb0374922a9e3073b01cd2ea1c71cd
Bettina Justus <bettina.jus@gmail.com>
2024-01-13 22:42:43 +0100

    Add supervisor endpoint to get all supervisors for list

---
+++ src/main/java/com/bamate/bamatebackend/account/SupervisorController.java
@@ -0,0 +1,23 @@
+package com.bamate.bamatebackend.account;
+
+import com.bamate.bamatebackend.account.models.Account;
+import com.bamate.bamatebackend.account.models.Role;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/supervisors")
+public class SupervisorController {
+    private final AccountRepository repository;
+
+    SupervisorController(AccountRepository repository) { this.repository = repository; }
+
+    // List content endpoint
+    @GetMapping()
+    public List<Account> supervisorAccounts() {
+        return repository.findByRole(Role.SUPERVISOR);
+    }
+}
