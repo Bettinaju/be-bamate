c783e67171e654e1e0692c2173e7400734824230
Bettina Justus <bettina.jus@gmail.com>
2024-01-13 22:40:32 +0100

    Add Docker to build endpoints with postgresql

--- src/main/java/com/bamate/bamatebackend/account/config/LoadAccountDatabase.java
+++ src/main/java/com/bamate/bamatebackend/account/config/LoadAccountDatabase.java
@@ -4,6 +4,7 @@ import com.bamate.bamatebackend.account.AccountRepository;
 import com.bamate.bamatebackend.account.models.Account;
 
 import com.bamate.bamatebackend.account.models.Role;
+import com.bamate.bamatebackend.account.models.Supervisor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -17,7 +18,15 @@ public class LoadAccountDatabase {
     @Bean
     CommandLineRunner initAccountDatabase(AccountRepository repository) {
         return args -> {
-            log.info("Preloading " + repository.save(new Account("betti@testl.com", "Betti", "12345678", Role.SUPERVISOR)));
+            log.info("Preloading " + repository.save(new Account("dami@test.com", "Dami", "Li", "12345678", Role.STUDENT)));
+            log.info("Preloading " + repository.save(new Account("freiheit@test.com", "Jörn", "Freiehit", "12345678", Role.ADMIN)));
+
+            log.info("Preloading " + repository.save(new Supervisor("betti@test.com", "Betti", "Ju", "12345678", Role.SUPERVISOR, "Bettis Description")));
+
+            Supervisor notAvailableSupervisor = new Supervisor("jana@test.com", "Jana", "Ko", "12345678", Role.SUPERVISOR, "Janas Description - I am not available");
+            notAvailableSupervisor.setAvailability(false);
+            log.info("Preloading " + repository.save(notAvailableSupervisor));
+
         };
     }
 
