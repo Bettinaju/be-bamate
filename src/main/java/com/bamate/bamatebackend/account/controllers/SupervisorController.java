3d88d604e91084b9ca1f51b9e3460018c1be762f
Bettina Justus <bettina.jus@gmail.com>
2024-02-05 20:06:29 +0100

    Fix empty fields from supervisors

--- src/main/java/com/bamate/bamatebackend/account/controllers/SupervisorController.java
+++ src/main/java/com/bamate/bamatebackend/account/controllers/SupervisorController.java
@@ -1,16 +1,14 @@
 package com.bamate.bamatebackend.account.controllers;
 
 import com.bamate.bamatebackend.account.AccountRepository;
-import com.bamate.bamatebackend.account.dto.SupervisorDTO;
+import com.bamate.bamatebackend.account.models.Account;
 import com.bamate.bamatebackend.account.models.Role;
-import com.bamate.bamatebackend.supervisor.models.Supervisor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * REST controller responsible for handling supervisor-related endpoints.
@@ -30,26 +28,7 @@ public class SupervisorController {
      */
     // List content endpoint
     @GetMapping()
-    public List<SupervisorDTO> supervisorAccounts() {
-        return repository.findByRole(Role.SUPERVISOR).stream()
-                .map(account -> convertToSupervisorDTO(new Supervisor(account)))
-                .collect(Collectors.toList());
-    }
-    /**
-     * Converts a {@code supervisor} object to a SupervisorDTO object.
-     * @param supervisor The {@code supervisor} object to convert.
-     * @return A SupervisorDTO object representing the {@code supervisor}.
-     */
-    private SupervisorDTO convertToSupervisorDTO(Supervisor supervisor) {
-        return new SupervisorDTO(
-                supervisor.getId(),
-                supervisor.getFirstName(),
-                supervisor.getLastName(),
-                supervisor.getEmail(),
-                supervisor.getRole(),
-                supervisor.getDescription(),
-                supervisor.isAvailable(),
-                supervisor.getInterests()
-        );
+    public List<Account> supervisorAccounts() {
+        return repository.findByRole(Role.SUPERVISOR);
     }
 }
